f9f81251d0b0fb5f1a4ac0e6cf52f0966c77633e
benjacas <castro.ben26@gmail.com>
2026-04-14 11:43:46 -0300

    modulo http

--- clase_14_04/index.mjs
+++ clase_14_04/index.mjs
@@ -1,9 +1,11 @@
 //modulo http
 import http from 'node:http'
+import fsp from 'node:fs/promises'
+import path from 'node:path'
 
 //creamos una instancia del servidor
-const app = http.createServer((peticion,respuesta)=>{
-    
+const app = http.createServer( async (peticion,respuesta)=>{
+if (peticion.method==='GET'){
 
     if (peticion.url==='/'){
         //antes del end todo, despues nada.
@@ -20,10 +22,24 @@ const app = http.createServer((peticion,respuesta)=>{
             'ruta usuarios /usuarios '
         )
     }
+}
+    
+    if(peticion.method==='POST'){
+        if(peticion.url === '/'){
 
+        const ruta = './contenido.txt'
+        await fsp.writeFile(ruta, 'contenido falso')
+            return respuesta.end('recurso creado')
+        }
+            
+    }
+//---------------------------------------------
+    //fallback
     respuesta.statusCode = 404
     return respuesta.end('No se encontró la ruta')
 })
 
 //abrimos puerto
-app.listen(3000,()=>)
+app.listen(3000,()=>{
+    console.log(`servidor escuchando en https://localhost:3000`)
+})

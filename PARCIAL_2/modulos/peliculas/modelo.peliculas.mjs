92aace78cb9438a21f4ae46f98b2f1929262f4bb
benjacas <castro.ben26@gmail.com>
2026-06-13 21:03:00 -0300

    dfd

---
+++ PARCIAL_2/modulos/peliculas/modelo.peliculas.mjs
@@ -0,0 +1,10 @@
+export const peliculas=[
+    {id: 1, titulo: "Inception", año: 2010, genero: "Ciencia Ficción"},
+    {id:2, titulo: "El padrino", año: 1972, genero: "Drama"},
+    {id:3, titulo: "Interestellar", año:"2014", genero: "Ciencia Ficción"}
+]
+
+export function ObtnerTodas(){
+    return peliculas
+}
+
---
+++ PARCIAL_2/peliculas.mjs
@@ -0,0 +1,14 @@
+export const peliculas = [
+    { id: 1,
+      titulo: "Inception",
+      año: 2010, 
+      genero: "Ciencia ficción" },
+    { id: 2, 
+      titulo: "El Padrino", 
+      año: 1972, 
+      genero: "Drama" },
+    { id: 3, 
+      titulo: "Interstellar", 
+      año: 2014, 
+      genero: "Ciencia ficción" }
+]
